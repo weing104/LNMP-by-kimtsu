881761ac28af7b205abe9f37701f0417e4370b6a
weing104 <bandwagonvps@163.com>
2016-08-16 12:46:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ clear
 echo "#################################################################"
 echo "#   Linux + Nginx + MySql + PHP with Control Panel  Installer   #"
 echo "#################################################################"
-echo "visit http://kimtsu.com"
+echo "visit http://imweb.cc"
 
 cur_dir=$(pwd)
 
@@ -328,78 +328,6 @@ chmod +x /etc/init.d/php53
 echo "============================PHP 5.3.29 install completed======================"
 }
 
-function InstallPHP-iwsv()
-{
-	echo "==========  InstallPHP-iwsv"
-echo "============================Install PHP 5.3.29================================"
-cd $cur_dir
-export PHP_AUTOCONF=/usr/local/autoconf-2.69/bin/autoconf
-export PHP_AUTOHEADER=/usr/local/autoconf-2.69/bin/autoheader
-cd php-iwsv/
-./configure --prefix=/usr/local/php-iwsv --with-config-file-path=/usr/local/php-iwsv/etc --enable-fpm --with-fpm-user=www --with-fpm-group=www --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-iconv-dir --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir=/usr --enable-xml --disable-rpath --enable-magic-quotes --enable-safe-mode --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --with-curlwrappers --enable-mbregex --enable-mbstring --with-mcrypt --enable-ftp --with-gd --enable-gd-native-ttf --with-openssl --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc --enable-zip --enable-soap --without-pear --with-gettext --disable-fileinfo
-
-make ZEND_EXTRA_LIBS='-liconv'
-make install
-
-echo "Copy new php configure file."
-mkdir -p /usr/local/php-iwsv/etc
-cp php.ini-production /usr/local/php-iwsv/etc/php.ini
-
-cd $cur_dir
-# php extensions
-echo "Modify php.ini......"
-sed -i 's/post_max_size = 8M/post_max_size = 50M/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/;date.timezone =/date.timezone = PRC/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/short_open_tag = Off/short_open_tag = On/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/; cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/; cgi.fix_pathinfo=0/cgi.fix_pathinfo=0/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/register_long_arrays = On/;register_long_arrays = On/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/magic_quotes_gpc = On/;magic_quotes_gpc = On/g' /usr/local/php-iwsv/etc/php.ini
-sed -i 's/disable_functions =.*/disable_functions = /g' /usr/local/php-iwsv/etc/php.ini
-
-echo "Install ZendGuardLoader for PHP 5.3"
-\cp -fr /usr/local/zend53 /usr/local/zend-iwsv
-echo "Write ZendGuardLoader to php.ini......"
-cat >>/usr/local/php-iwsv/etc/php.ini<<EOF
-;eaccelerator
-
-;ionCube
-
-[Zend Optimizer] 
-zend_extension=/usr/local/zend-iwsv/ZendGuardLoader.so
-EOF
-
-echo "Creating new php-fpm configure file......"
-cat >/usr/local/php-iwsv/etc/php-fpm.conf<<EOF
-[global]
-pid = /usr/local/php-iwsv/var/run/php-fpm.pid
-error_log = /usr/local/php-iwsv/var/log/php-fpm.log
-log_level = notice
-
-[www]
-listen = /tmp/php-iwsv.sock
-listen.mode = 0666
-user = root
-group = root
-pm = dynamic
-pm.max_children = 10
-pm.start_servers = 2
-pm.min_spare_servers = 1
-pm.max_spare_servers = 6
-request_terminate_timeout = 100
-EOF
-
-echo "Copy php-fpm init.d file......"
-cp $cur_dir/php-iwsv/sapi/fpm/init.d.php-fpm /etc/init.d/php-iwsv
-sed -i 's/php_opts="/php_opts="-R /g' /etc/init.d/php-iwsv
-chmod +x /etc/init.d/php-iwsv
-
-echo "============================PHP 5.3.29 install completed======================"
-}
-
 function InstallNginx()
 {
 	echo "==========  InstallNginx"
@@ -456,45 +384,6 @@ unzip $cur_dir/webdefault.zip -d /home/wwwroot/default
 chown -R www:www /home/wwwroot/default
 }
 
-function Nginx-iwsv()
-{
-	echo "==========  InstallNginx"
-echo "============================Install Nginx================================="
-cd $cur_dir
-
-cd nginx-iwsv/
-sed -i 's/Server: /Server: ImWeb-/g' src/http/ngx_http_header_filter_module.c
-unzip $cur_dir/ngx_http_auth_pam_module-master.zip
-./configure --user=www --group=www --prefix=/usr/local/nginx-iwsv --with-http_stub_status_module --with-http_ssl_module --with-http_gzip_static_module --with-ipv6 --add-module=ngx_http_auth_pam_module-master
-make && make install
-cd ../
-
-ln -s /usr/local/nginx-iwsv/sbin/nginx /usr/bin/nginx-iwsv
-
-rm -f /usr/local/nginx-iwsv/conf/nginx.conf
-rm -f /usr/local/nginx-iwsv/conf/fcgi.conf
-unzip nginx-iwsv_conf.zip -d /usr/local/nginx-iwsv/conf/
-mkdir /usr/local/nginx-iwsv/conf/rewrite
-unzip $cur_dir/rewrite.zip -d /usr/local/nginx-iwsv/conf/rewrite
-
-cd $cur_dir
-
-
-mkdir -p /home/nginx-iwsv/
-chmod +w /home/nginx-iwsv/
-tar zxvf $cur_dir/nginx-iwsv.tar.gz -C /home/nginx-iwsv/
-\rm -fr /home/nginx-iwsv/config/mysqlroot.php
-cat >>/home/nginx-iwsv/config/mysqlroot.php<<eof
-<?php \$mysqlroot = '$mysqlrootpwd';
-eof
-
-cat >>/etc/pam.d/nginx<<eof
-auth    required     pam_unix.so
-account required     pam_unix.so
-eof
-
-}
-
 function CreatPHPTools()
 {
 	echo "==========  CreatPHPTools"
@@ -534,8 +423,6 @@ chmod +x /etc/init.d/nginx-iwsv
 chkconfig --level 345 crond on
 chkconfig --level 345 php53 on
 chkconfig --level 345 nginx on
-chkconfig --level 345 php-iwsv on
-chkconfig --level 345 nginx-iwsv on
 chkconfig --level 345 mysql on
 chkconfig --level 345 postfix off
 echo "===========================add nginx and php-fpm on startup completed===================="
@@ -544,8 +431,6 @@ echo "Starting LNMP..."
 /etc/init.d/mysql start
 /etc/init.d/php53 start
 /etc/init.d/nginx start
-/etc/init.d/php-iwsv start
-/etc/init.d/nginx-iwsv start
 /etc/init.d/postfix stop
 
 #add 80 port to iptables
@@ -614,38 +499,6 @@ if [ -s /sbin/iptables ]; then
 /sbin/iptables -I INPUT -p tcp --dport 20 -j ACCEPT
 /sbin/iptables-save
 fi
-
-}
-
-function IntallionCube()
-{
-	echo "==========  IntallionCube"
-cd $cur_dir
-
-tar zxvf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local/
-cd /usr/local/
-sed -i '/ionCube Loader/d' /usr/local/php53/etc/php.ini
-sed -i '/ionCube Loader/d' /usr/local/php-iwsv/etc/php.ini
-sed -i '/ioncube_loader_lin/d' /usr/local/php53/etc/php.ini
-sed -i '/ioncube_loader_lin/d' /usr/local/php-iwsv/etc/php.ini
-
-cat >ionCube53.ini<<EOF
-[ionCube Loader]
-zend_extension="/usr/local/ioncube/ioncube_loader_lin_5.3.so"
-EOF
-
-sed -i '/;ionCube/ {
-r ionCube53.ini
-}' /usr/local/php-iwsv/etc/php.ini
-sed -i '/;ionCube/ {
-r ionCube53.ini
-}' /usr/local/php53/etc/php.ini
-
-echo "Restarting php-fpm......"
-RestartAllPHP
-
-rm ionCube53.ini
-cd $cur_dir
 
 }
 
@@ -736,7 +589,6 @@ function RestartAllPHP()
 {
 	echo "==========  RestartAllPHP"
 	/etc/init.d/php53 restart
-	/etc/init.d/php-iwsv restart
 }
 
 function Install_PHPMemcache()
@@ -820,11 +672,8 @@ checkFile xcache-3.2.0.tar.gz
 checkFile memcache-3.0.8.tgz
 checkFile memcached-1.4.25.tar.gz
 checkFile init.d.memcached
-checkFile init.d.nginx-iwsv
-checkFile nginx-iwsv_conf.zip
 checkFile init.d.pureftpd
 checkFile ngx_http_auth_pam_module-master.zip
-checkFile nginx-iwsv.tar.gz
 checkFile rewrite.zip
 checkFile lua-nginx-module-master.zip
 checkFile lua-5.3.3.tar.gz
@@ -838,14 +687,11 @@ checkFile ngx_http_substitutions_filter_module-master.zip
 InstallDependsAndOpt
 InstallMySQL55
 InstallPHP53
-InstallPHP-iwsv
 InstallNginx
-Nginx-iwsv
 CreatPHPTools
 AddAndStartup
 IntallFTP
 installXcahe53
-IntallionCube
 RestartAllPHP
 Install_PHPMemcache
 Install_PHPMemcache53
